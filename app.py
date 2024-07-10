463c36337ab83ab6cadc4ae9761d836a97e59241
Mili Kotnala <kotnalamili1@gmail.com>
2024-07-10 19:54:31 +0530

    Update2 app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-pip install streamlit pandas nltk
+
 # Ensure nltk resources are downloaded
 nltk.download('stopwords')
 nltk.download('punkt')
